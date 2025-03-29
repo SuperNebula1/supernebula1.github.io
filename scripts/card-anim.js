8eb27e5f532b2fdd3d0488acdb9c514d802855ee
Tanner Adolphe <tan.lego05@gmail.com>
2025-03-29 08:07:32 -0400

    First Commit

---
+++ scripts/card-anim.js
@@ -0,0 +1,38 @@
+document.querySelectorAll('.project').forEach(card => {
+    const glow = card.querySelector('.glow-border');
+  
+    card.addEventListener('mousemove', (e) => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+  
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+  
+      const angle = Math.atan2(y - centerY, x - centerX) * (180 / Math.PI) + 180;
+  
+      const spread = 280; // degrees
+      const grad = [];
+  
+      for (let i = 0; i <= 360; i++) {
+        const diff = Math.abs(i - angle) > 180
+          ? 360 - Math.abs(i - angle)
+          : Math.abs(i - angle);
+  
+        const alpha = Math.max(0, 1.5 - diff / (spread / 2));
+  
+        if (localStorage.getItem('theme') === 'dark') {
+          grad.push(`rgba(0, 255, 255, ${alpha.toFixed(2)}) ${i}deg`);
+        } else {
+          grad.push(`rgba(59, 92, 204, ${alpha.toFixed(2)}) ${i}deg`);
+        }
+      }
+  
+      glow.style.background = `conic-gradient(from 0deg at 50% 50%, ${grad.join(", ")})`;
+    });
+  
+    card.addEventListener('mouseleave', () => {
+      glow.style.background = "transparent";
+    });
+  });
+  
---
+++ scripts/script.js
@@ -0,0 +1,97 @@
+var theme = localStorage.getItem('theme');
+var r = document.querySelector(':root');
+var rs = getComputedStyle(r);
+
+var pathLight = "images/hero-dark.svg";
+var pathDark = "images/hero-light.svg";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.getElementById('colourToggle');
+  const sun = document.getElementById('sun');
+  const moon = document.getElementById('moon');
+  const heroImg = document.getElementById('heroImg');
+  if(theme === 'dark') {
+    r.style.setProperty('--colour-bg1', rs.getPropertyValue('--colour-dark-bg1'));
+    r.style.setProperty('--colour-bg2', rs.getPropertyValue('--colour-dark-bg2'));
+    r.style.setProperty('--colour-text', rs.getPropertyValue('--colour-dark-text'));
+    r.style.setProperty('--colour-btn1-bg', rs.getPropertyValue('--colour-dark-btn1-bg'));
+    r.style.setProperty('--colour-btn2-bg', rs.getPropertyValue('--colour-dark-btn2-bg'));
+    r.style.setProperty('--colour-btn1-text', rs.getPropertyValue('--colour-dark-btn1-text'));
+    r.style.setProperty('--colour-btn2-text', rs.getPropertyValue('--colour-dark-btn2-text'));
+    r.style.setProperty('--colour-card-border', rs.getPropertyValue('--colour-card-dark-border'));
+    button.classList.add('dark');
+    sun.classList.add('dark');
+    moon.classList.add('dark');
+    heroImg.src = pathDark;
+    
+  } 
+  else {
+      r.style.setProperty('--colour-bg1', rs.getPropertyValue('--colour-light-bg1'));
+      r.style.setProperty('--colour-bg2', rs.getPropertyValue('--colour-light-bg2'));
+      r.style.setProperty('--colour-text', rs.getPropertyValue('--colour-light-text'));
+      r.style.setProperty('--colour-btn1-bg', rs.getPropertyValue('--colour-light-btn1-bg'));
+      r.style.setProperty('--colour-btn2-bg', rs.getPropertyValue('--colour-light-btn2-bg'));
+      r.style.setProperty('--colour-btn1-text', rs.getPropertyValue('--colour-light-btn1-text'));
+      r.style.setProperty('--colour-btn2-text', rs.getPropertyValue('--colour-light-btn2-text'));
+      r.style.setProperty('--colour-card-border', rs.getPropertyValue('--colour-card-light-border'));
+      button.classList.add('light');
+      heroImg.src = pathLight;
+
+  }
+  
+
+});
+
+
+
+
+function colourToggle() {
+  
+  theme = localStorage.getItem('theme');
+  const button = document.getElementById('colourToggle');
+  const sun = document.getElementById('sun');
+  const moon = document.getElementById('moon');
+  const heroImg = document.getElementById('heroImg');
+  if(theme === 'dark') {
+    localStorage.setItem('theme', 'light');
+    r.style.setProperty('--colour-bg1', rs.getPropertyValue('--colour-light-bg1'));
+    r.style.setProperty('--colour-bg2', rs.getPropertyValue('--colour-light-bg2'));
+    r.style.setProperty('--colour-text', rs.getPropertyValue('--colour-light-text'));
+    r.style.setProperty('--colour-btn1-bg', rs.getPropertyValue('--colour-light-btn1-bg'));
+    r.style.setProperty('--colour-btn2-bg', rs.getPropertyValue('--colour-light-btn2-bg'));
+    r.style.setProperty('--colour-btn1-text', rs.getPropertyValue('--colour-light-btn1-text'));
+    r.style.setProperty('--colour-btn2-text', rs.getPropertyValue('--colour-light-btn2-text'));
+    r.style.setProperty('--colour-card-border', rs.getPropertyValue('--colour-card-light-border'));
+    r.style.transition = 'background-color 0.5s ease';
+    button.classList.replace('dark','light');
+    sun.classList.remove('dark');
+    moon.classList.remove('dark');
+    heroImg.src = pathLight;
+
+    return;
+  } else if(theme === 'light') {
+
+
+    localStorage.setItem('theme', 'dark');
+    r.style.setProperty('--colour-bg1', rs.getPropertyValue('--colour-dark-bg1'));
+    r.style.setProperty('--colour-bg2', rs.getPropertyValue('--colour-dark-bg2'));
+    r.style.setProperty('--colour-text', rs.getPropertyValue('--colour-dark-text'));
+    r.style.setProperty('--colour-btn1-bg', rs.getPropertyValue('--colour-dark-btn1-bg'));
+    r.style.setProperty('--colour-btn2-bg', rs.getPropertyValue('--colour-dark-btn2-bg'));
+    r.style.setProperty('--colour-btn1-text', rs.getPropertyValue('--colour-dark-btn1-text'));
+    r.style.setProperty('--colour-btn2-text', rs.getPropertyValue('--colour-dark-btn2-text'));
+    r.style.setProperty('--colour-card-border', rs.getPropertyValue('--colour-card-dark-border'));
+    r.style.transition = 'background-color 0.5s ease';
+    button.classList.replace('light','dark');
+    sun.classList.add('dark');
+    moon.classList.add('dark');
+    heroImg.src = pathDark;
+    
+    return;
+  }
+  
+
+}
+
+
